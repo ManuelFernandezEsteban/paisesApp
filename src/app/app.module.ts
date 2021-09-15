6d1c6fd116370626a24e22381f61321357bc086b
Manuel1977 <Manuel_fe1977@hotmail.com>
2021-09-15 23:19:41 +0200

    componente por pais y servicio pais

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import {HttpClientModule} from '@angular/common/http'
+
 
 import { AppComponent } from './app.component';
 import { PaisModule } from './pais/pais.module';
@@ -12,9 +14,11 @@ import { SharedModule } from './shared/shared.module';
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
     PaisModule,
     SharedModule,
-    AppRoutingModule
+    
 
   ],
   
